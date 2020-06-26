50cacff76b9abf27a62df36fa59fb153e1909e65
Ian Dickinson <i.j.dickinson@gmail.com>
2020-06-26 09:24:59 +0100

    Add a simple arrow marker to the edge line

--- src/components/GraphEdge.vue
+++ src/components/GraphEdge.vue
@@ -1,11 +1,25 @@
 <template>
-  <line
-    :x1='startX'
-    :y1='startY'
-    :x2='endX'
-    :y2='endY'
-    class='c-graph-edge'
-  />
+  <g>
+    <marker
+      id='arrow'
+      viewBox='0 0 10 10'
+      refX='5' refY='5'
+      markerWidth='6'
+      markerHeight='6'
+      orient='auto-start-reverse'
+    >
+      <path d="M 0 0 L 10 5 L 0 10 z" />
+    </marker>
+    <line
+      :x1='startX'
+      :y1='startY'
+      :x2='endX'
+      :y2='endY'
+      class='c-graph-edge'
+      marker-end='url("#arrow")'
+    />
+
+  </g>
 </template>
 
 <script>
